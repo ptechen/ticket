bf6ecaa4755fbbb213e979186abf44306afba30e
陶辰 <chen.tao@ifchange.com>
2019-07-31 21:37:10 +0800

    update

--- app.py
+++ app.py
@@ -440,20 +440,20 @@ class CrackTouClick(object):
 
 
 if __name__ == '__main__':
-    params = {"account": "pychance", "password": "TC15736755067", "email": '15736755067@163.com',
-                       "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38948",
-                       "input_card_number": "379387027461007", "input_security_code": 9549,
-                       "payment_expiry_month_select": "05", "payment_expiry_year_select": 2024, "period": 1,
-              "start_time": 1564624500
-                       }
-    # params1 = {"account": "taotao123", "password": "taotao123", "email": '15736755067@163.com',
-    #           "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38948",
-    #           "input_card_number": "379387027461007", "input_security_code": 9549,
-    #           "payment_expiry_month_select": "05", "payment_expiry_year_select": 2024, "period": 1,
+    # params = {"account": "pychance", "password": "TC15736755067", "email": '15736755067@163.com',
+    #                    "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38948",
+    #                    "input_card_number": "379387027461007", "input_security_code": 9549,
+    #                    "payment_expiry_month_select": "05", "payment_expiry_year_select": 2024, "period": 1,
     #           "start_time": 1564624500
-    #           }
-    redisConn = RedisConn()
-    redisConn.write(params)
+    #                    }
+    # # params1 = {"account": "taotao123", "password": "taotao123", "email": '15736755067@163.com',
+    # #           "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38948",
+    # #           "input_card_number": "379387027461007", "input_security_code": 9549,
+    # #           "payment_expiry_month_select": "05", "payment_expiry_year_select": 2024, "period": 1,
+    # #           "start_time": 1564624500
+    # #           }
+    # redisConn = RedisConn()
+    # redisConn.write(params)
     # redisConn.write(params1)
     # while time.time() < 1564624500:
     #     time.sleep(1)
