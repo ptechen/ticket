7efd55b1d1ba2cd4c8b44897335f601059ae7421
陶辰 <chen.tao@ifchange.com>
2019-07-31 21:26:27 +0800

    update

--- app1.py
+++ app1.py
@@ -14,21 +14,21 @@ from chaojiying import Chaojiying
 import redis
 import json
 
-
-
 CHAOJIYING_USERNAME = '15736755067'
 CHAOJIYING_PASSWORD = 'TC15736755067'
 CHAOJIYING_SOFT_ID = 899053
 CHAOJIYING_KIND = 1902
 CHAOJIYING_KIND1 = 1005
 chrome_options = webdriver.ChromeOptions()
-#无头模式
+# 无头模式
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('window-size=1920,1080')
 chrome_options.add_argument('--disable-gpu')
-#实例化Chrome driver823
+
+
+# 实例化Chrome driver823
 # driver=webdriver.Chrome(chrome_options=chrome_options)
 
 
@@ -41,12 +41,13 @@ class RedisConn(object):
         params_str = json.dumps(params)
         self.red.sadd("key", params_str)
 
+
 class CrackTouClick(object):
     def __init__(self):
         self.url = 'http://www.baidu.com'
         self.KeyUrl = 'https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38663'
-        self.browser = webdriver.Chrome(options=chrome_options)
-        self.browser_headless = True
+        self.browser = webdriver.Chrome()
+        self.browser_headless = False
         self.wait = WebDriverWait(self.browser, 20)
         self.chaojiying = Chaojiying(CHAOJIYING_USERNAME, CHAOJIYING_PASSWORD, CHAOJIYING_SOFT_ID)
         # self.pool = redis.ConnectionPool(host='47.56.66.23', password='123456', port=6379, db=0, max_connections=2)
@@ -54,7 +55,7 @@ class CrackTouClick(object):
         self.params = None
 
     def __del__(self):
-            self.browser.close()
+        self.browser.close()
 
     def get_redis(self):
         flage = True
@@ -82,14 +83,12 @@ class CrackTouClick(object):
         spw = self.wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="1"]/h3/a[1]')))
         spw.click()
         self.browser.switch_to.window(self.browser.window_handles[1])
+        print(self.browser.current_url)
+        time.sleep(4)
         if self.browser.current_url == "https://busy.urbtix.hk/":
             time.sleep(4)
         if self.browser.current_url == "https://busy.urbtix.hk/redirect.html":
-            time.sleep(4)
-            try:
-                self.browser.find_element_by_xpath('/html/body/div[2]/table/tbody/tr/td/table/tbody/tr[4]/td/a').Click()
-            except Exception as E:
-                print(E)
+            self.browser.find_element_by_xpath('/html/body/div[2]/table/tbody/tr/td/table/tbody/tr[4]/td/a').Click()
 
     def login(self):
         self.browser.get('https://ticket.urbtix.hk/internet/login/memberLogin')
@@ -117,11 +116,15 @@ class CrackTouClick(object):
             element = self.browser.find_element_by_xpath('//*[@id="captchaImage"]')
             # element = self.wait.until(EC.presence_of_element_located((By.ID, 'captchaImage')))
         elif kind == 2:
-            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#captcha-image-input-key-container > table > tbody')))
-            element = self.browser.find_element_by_xpath('//*[@id="captcha-image-input-key-container"]/table/tbody/tr[1]')
+            self.wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#captcha-image-input-key-container > table > tbody')))
+            element = self.browser.find_element_by_xpath(
+                '//*[@id="captcha-image-input-key-container"]/table/tbody/tr[1]')
         elif kind == 3:
-            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#captcha-image-input-key-container > table > tbody')))
-            element = self.browser.find_element_by_xpath('//*[@id="captcha-image-input-key-container"]/table/tbody/tr[2]')
+            self.wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#captcha-image-input-key-container > table > tbody')))
+            element = self.browser.find_element_by_xpath(
+                '//*[@id="captcha-image-input-key-container"]/table/tbody/tr[2]')
         return element
 
     def get_position(self, kind):
@@ -225,13 +228,16 @@ class CrackTouClick(object):
         return result['pic_str']
 
     def refresh(self):
-        self.browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/table/tbody/tr/td[1]/table/tbody/tr[8]/td[2]/img[2]').click()
+        self.browser.find_element_by_xpath(
+            '//*[@id="loginForm"]/div/div[2]/table/tbody/tr/td[1]/table/tbody/tr[8]/td[2]/img[2]').click()
         time.sleep(2)
 
     def click_code(self, key_index):
         for index in key_index:
             if index < 5:
-                self.browser.find_element_by_xpath('//*[@id="captcha-image-input-key-container"]/table/tbody/tr[1]/td[{}]/img'.format(index + 1)).click()
+                self.browser.find_element_by_xpath(
+                    '//*[@id="captcha-image-input-key-container"]/table/tbody/tr[1]/td[{}]/img'.format(
+                        index + 1)).click()
             else:
                 index = index - 5
                 self.browser.find_element_by_xpath(
@@ -250,7 +256,8 @@ class CrackTouClick(object):
                 self.browser.get(self.params.get("key_url"))
             try:
                 time.sleep(0.1)
-                self.browser.find_element_by_xpath('//*[@id="evt-perf-items-tbl"]/tbody/tr[{}]/td[6]/div/img'.format(self.params.get('period'))).click()
+                self.browser.find_element_by_xpath('//*[@id="evt-perf-items-tbl"]/tbody/tr[{}]/td[6]/div/img'.format(
+                    self.params.get('period'))).click()
                 break
             except Exception as E:
                 pass
@@ -273,7 +280,7 @@ class CrackTouClick(object):
         try:
             self.browser.find_element_by_xpath('/html/body/div[9]/div[3]/div/button[1]').click()
         except Exception as E:
-        #     self.browser.find_element_by_xpath('//*[@id="reviewTicketForm"]/div[8]/div/div').click()
+            #     self.browser.find_element_by_xpath('//*[@id="reviewTicketForm"]/div[8]/div/div').click()
             print(E)
         print("end choose_ticket_num")
         self.browser.save_screenshot("choose_ticket_num.png")
@@ -441,23 +448,15 @@ class CrackTouClick(object):
 
 if __name__ == '__main__':
     params = {"account": "pychance", "password": "TC15736755067", "email": '15736755067@163.com',
-                       "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38948",
-                       "input_card_number": "379387027461007", "input_security_code": 9549,
-                       "payment_expiry_month_select": "05", "payment_expiry_year_select": 2024, "period": 1,
-              "start_time": 1564624500
-                       }
-    params1 = {"account": "taotao123", "password": "taotao123", "email": '15736755067@163.com',
-              "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38948",
+              "key_url": "https://ticket.urbtix.hk/internet/zh_TW/eventDetail/38382",
               "input_card_number": "379387027461007", "input_security_code": 9549,
               "payment_expiry_month_select": "05", "payment_expiry_year_select": 2024, "period": 1,
               "start_time": 1564624500
               }
-    redisConn = RedisConn()
-    redisConn.write(params)
-    redisConn.write(params1)
-    # while time.time() < 1564624500:
-    #     time.sleep(1)
 
+    redisConn = RedisConn()
+
+    redisConn.write(params)
     crack = CrackTouClick()
     crack.login_crack()
 # 'http://msg.urbtix.hk/'
